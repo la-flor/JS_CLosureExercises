b3c948b4b39ad5015a750f8ec1743e671203e0cd
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-20 10:20:58 -0600

    Completed guessingGame()

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
-
-}
-
-module.exports = { guessingGame };
+    const winningNum = Math.floor(Math.random() * 100);
+    let isOver = false;
+    let totalGuesses = 0;
+  
+    return function guess(num) {
+      if (isOver) return "The game is over, you already won!";
+      totalGuesses++;
+      if (num === winningNum) {
+        isOver = true;
+        const guess = totalGuesses === 1 ? "guess" : "guesses";
+        return `You win! You found ${num} in ${totalGuesses} ${guess}.`;
+      }
+      if (num < winningNum) return `${num} is too low!`;
+      if (num > winningNum) return `${num} is too high!`;
+    };
+  }
+  
+  module.exports = { guessingGame };
